621466b4a96544912e80059de238f033e0a20403
hoanpm <hoan922003@gmail.com>
2023-05-09 20:24:48 +0700

    songbar

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -2,12 +2,14 @@ function Card({ img, name, desc }) {
     return (
         <div className="card">
             <img src={img}  alt="img" className="card-img"/>
-  
+            <a href="https://boxicons.com/?query=volume"></a>
             <div className="card-body">
                 <div className="name"><h2>{name}</h2></div>
                 <div className="desc"><p>{desc}</p></div>
             </div>
         </div>
+
+        
     )
 }
 
--- client/src/pages/HomePage/PageContent.js
+++ client/src/pages/HomePage/PageContent.js
@@ -11,8 +11,9 @@ function PageContent({children}) {
     const sidebar = useRef();
     const searchBox = useRef();
     const navigate = useRef();
+    const player = useRef();
 
-    useEffect(() => {
+        useEffect(() => {
         if (location.pathname === "/search") {
             searchBox.current.style.display = "";
         } else searchBox.current.style.display = "none";
@@ -51,9 +52,73 @@ function PageContent({children}) {
                 </div>
             </div>
             {children}
-            <div className="Player">
-                    
+            
+            <footer class="bottom" id="bottom-click">
+	            <div class="active-song-description">
+                <div id="song-image">
+                    <img src="https://www.lololyrics.com/img/cover/39824.jpeg"/>
+                </div>
+                <div class="song-desc">
+                    <div>
+                        Be Kind
+                    </div>
+                    <div >
+                        Halsey
+                    </div>
+                </div>
+                <div class="heart-and-ban-icon">
+                <span>
+                    <i class='bx bx-heart'></i>
+                </span>
+                
+                <span>
+                    <i class="fas fa-ban"></i>
+                </span>
             </div>
+        </div>
+    <div class="player">
+            <div class="controls">
+                <div><i class='bx bx-shuffle'></i></div>
+                <div><i class='bx bx-skip-previous'></i></div>
+                <div><i class='bx bx-pause'></i></div>
+                <div><i class='bx bx-skip-next'></i></div>
+                <div><i class='bx bx-sync'></i></div>
+            </div>
+            <div id="slider">
+                <div class="time">
+                    0:00
+                </div>
+
+                <div class="slidecontainer">
+                    <input type="range" min="0" max="100" value="0" class="slider" id="myRange" style={{ width: "100%" }}/>
+                </div>
+                <div class="time">
+                    5:10
+                </div>
+            </div>
+
+        </div>
+
+        <div class="extras">
+            <div>
+                <i class='bx bx-list-ul'></i>
+            </div>
+            <div>
+                <i class='bx bx-laptop'></i>
+            </div>
+            <div>
+                <i class='bx bxs-volume-full'></i>
+            </div>
+            <div class="slidecontainer" style={{ width: "30%" , marginTop:"-10px"  }}>
+                <input type="range" min="0" max="100" value="0" class="slider" id="myRange" style={{marginTop:"0px" , width: "100%" }}/>
+            </div>
+                    <div>
+                        <i class="fas fa-expand-alt"></i>
+                    </div>
+                </div>
+
+
+            </footer>
         </div>
     </div>
     )
