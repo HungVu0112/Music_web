11d5896628859111c1b0bd07b7fd3801306546b8
hoanpm <hoan922003@gmail.com>
2023-05-11 00:09:28 +0700

    songcard

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,16 +1,13 @@
 function Card({ img, name, desc }) {
     return (
-        <div className="card">
+        <div >
             <img src={img}  alt="img" className="card-img"/>
             <a href="https://boxicons.com/?query=volume"></a>
-  
             <div className="card-body">
                 <h2 className="name">{name}</h2>
                 <p className="desc">{desc}</p>
             </div>
         </div>
-
-        
     )
 }
 
