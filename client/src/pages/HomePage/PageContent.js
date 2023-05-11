5bbceeffd26e836dbf2a9f1ae6fe752b388ef9aa
HungVu0112 <100528129+HungVu0112@users.noreply.github.com>
2023-05-11 15:30:58 +0700

    fix gub commit

--- client/src/pages/HomePage/PageContent.js
+++ client/src/pages/HomePage/PageContent.js
@@ -37,7 +37,7 @@ function PageContent({children}) {
         if (location.pathname.includes("/playlists/")) {
             redirectBack2.current.style.display = "";
         } else redirectBack2.current.style.display = "none";
-        
+
     },[location.pathname])
 
     return (
@@ -93,61 +93,50 @@ function PageContent({children}) {
                 <span>
                     <i className="fas fa-ban"></i>
                 </span>
-            </div>
-        </div>
-<<<<<<< HEAD
-    <div class="player">
-            <div class="controls">
-                <i class='bx bx-shuffle'></i>
-                <i class='bx bx-skip-previous'></i>
-                <i class='bx bx-pause'></i>
-                <i class='bx bx-skip-next'></i>
-                <i class='bx bx-sync'></i>
-=======
-    <div className="player">
-            <div className="controls">
-                <div><i className='bx bx-shuffle'></i></div>
-                <div><i className='bx bx-skip-previous'></i></div>
-                <div><i className='bx bx-pause'></i></div>
-                <div><i className='bx bx-skip-next'></i></div>
-                <div><i className='bx bx-sync'></i></div>
->>>>>>> 2fb0cbc3a4665c59ed42124868e5fc95f40ed9b9
-            </div>
-            <div id="slider">
-                <div className="time">
-                    0:00
-                </div>
-
-                <div className="slidecontainer">
-                    <input type="range" min="0" max="100" value="0" className="slider" id="myRange" style={{ width: "100%" }} onChange={() => {}}/>
-                </div>
-                <div className="time">
-                    5:10
                 </div>
             </div>
 
-        </div>
+            <div className="player">
+                <div className="controls">
+                    <div><i className='bx bx-shuffle'></i></div>
+                    <div><i className='bx bx-skip-previous'></i></div>
+                    <div><i className='bx bx-pause'></i></div>
+                    <div><i className='bx bx-skip-next'></i></div>
+                    <div><i className='bx bx-sync'></i></div>
+                </div>
+                <div id="slider">
+                    <div className="time">
+                        0:00
+                    </div>
 
-        <div className="extras">
-            <div>
-                <i className='bx bx-list-ul'></i>
-            </div>
-            <div>
-                <i className='bx bx-laptop'></i>
-            </div>
-            <div>
-                <i className='bx bxs-volume-full'></i>
-            </div>
-            <div className="slidecontainer" style={{ width: "30%" , marginTop:"-10px"  }}>
-                <input type="range" min="0" max="100" value="0" className="slider" id="myRange" style={{marginTop:"0px" , width: "100%" }} onChange={() => {}}/>
-            </div>
-                    <div>
-                        <i className="fas fa-expand-alt"></i>
+                    <div className="slidecontainer">
+                        <input type="range" min="0" max="100" value="0" className="slider" id="myRange" style={{ width: "100%" }} onChange={() => {}}/>
+                    </div>
+                    <div className="time">
+                        5:10
                     </div>
                 </div>
 
+            </div>
 
-            </footer>
+            <div className="extras">
+                <div>
+                    <i className='bx bx-list-ul'></i>
+                </div>
+                <div>
+                    <i className='bx bx-laptop'></i>
+                </div>
+                <div>
+                    <i className='bx bxs-volume-full'></i>
+                </div>
+                <div className="slidecontainer" style={{ width: "30%" , marginTop:"-10px"  }}>
+                    <input type="range" min="0" max="100" value="0" className="slider" id="myRange" style={{marginTop:"0px" , width: "100%" }} onChange={() => {}}/>
+                </div>
+                        <div>
+                            <i className="fas fa-expand-alt"></i>
+                        </div>
+            </div>
+        </footer>
         </div>
     </div>
     )
